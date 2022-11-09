7e66513f88c350f79c01d0c193d4f417450d697b
Michelle-Wanderi <michellewangari12@gmail.com>
2022-11-09 06:18:05 +0300

    exercises

--- main.js
+++ main.js
@@ -85,5 +85,63 @@ for (let i = 0; i < bobsFollowers.length; i++) {
   }
 };
 
+// //While loop
+// 2.
+// Create a while loop with a condition that checks if the currentCard does not have that value 'spade'.
+// Inside the block of your while loop, add the following line of code:
+// currentCard = cards[Math.floor(Math.random() * 4)];
+// Math.floor(Math.random() * 4) will give us a random number from 0 to 3. 
+// We’ll use this number to index the cards array, and assign the value of currentCard to a random element from that array.
 
 
+
+const cards = ['diamond', 'spade', 'heart', 'club'];
+let currentCard;
+
+while ( currentCard != 'spade') {
+  currentCard = cards[Math.floor(Math.random() * 4)];
+	console.log(currentCard);
+}
+
+// Do while
+
+// We’d like a program to simulate part of the cake-baking process. Depending on the recipe, 
+// a different number of cups of sugar is required. Create the variable cupsOfSugarNeeded,
+//  and assign it a number value of your choosing.
+
+// // The cups of sugar must be added to the batter one at a time.
+//  Declare the variable cupsAdded and assign it the value 
+// // We have a sweet tooth, so we want to add at least one cup of sugar to the batter even if the value of cupsOfSugarNeeded is 0.
+
+// // Create a do...while loop which increments cupsAdded by one while cupsAdded is less than cupsOfSugarNeeded.
+
+// // In order to help us visualize the output on each iteration of the loop, add a console.log within the do...while 
+// block and log the value of cupsAdded.
+
+let cupsOfSugarNeeded = 3;
+let cupsAdded = 0;
+
+do {
+ cupsAdded++
+
+} while (cupsAdded < cupsOfSugarNeeded);
+
+// For loop in array that has a break statement 
+// the break statement exits the loop when the condition is met
+
+const rapperArray = ["Lil' Kim", "Jay-Z", "Notorious B.I.G.", "Tupac"];
+
+// Write your code below
+for (let i = 0; i < rapperArray.length; i++){
+  console.log(rapperArray[i]);
+  if (rapperArray[i] === 'Notorious B.I.G.'){
+    break;
+  }
+}
+
+console.log("And if you don't know, now you know.");
+
+
+//For each method
+const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+fruits.forEach(fruit => console.log(`I want to eat a ${fruit}`));
