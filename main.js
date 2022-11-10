fa68568f29ce00bd620dcfa77287343799c49c2d
Michelle-Wanderi <michellewangari12@gmail.com>
2022-11-10 07:12:26 +0300

    exercises

--- main.js
+++ main.js
@@ -205,3 +205,146 @@ console.log(newSum);
 // .filter() checks every element in an array to see if it meets certain criteria and returns a new array with the elements that return truthy for the criteria.
 // .findIndex() returns the index of the first element of an array that satisfies a condition in the callback function. It returns -1 if none of the elements in the array satisfies the condition.
 // .reduce() iterates through an array and takes the values of the elements and returns a single value.
+
+//Creating objects
+let fasterShip = {
+  'Fuel Type' : 'Turbo Fuel',
+  'color' : 'silver'
+};     //Syntax for an object literal
+
+
+let spaceship = {
+  'Fuel Type' : 'Turbo Fuel',
+  'Active Mission' : true,
+  homePlanet : 'Earth', 
+  numCrew: 5
+ };
+const isActive = spaceship['Active Mission']
+let propName =  'Active Mission';
+console.log(spaceship, 'Active Mission');    //Returns the value of the active mission property
+
+//Including methods in an object
+let retreatMessagee = 'We no longer wish to conquer your planet. It is full of dogs, which we do not care for.';
+
+let alienShipp = {
+retreat() {
+  console.log(retreatMessage);
+}
+};
+
+let retreatMessage = 'We no longer wish to conquer your planet. It is full of dogs, which we do not care for.';
+
+// Write your code below
+
+let alienShip = {
+  retreat() {
+    console.log(retreatMessage)
+  },
+  takeOff() {
+    console.log('Spim... Borp... Glix... Blastoff!')
+  }
+};
+
+alienShip.retreat();
+
+alienShip.takeOff();
+
+
+let spaceshipp = {
+  passengers: [{name: 'Space Dog'}], //An array of objects
+  telescope: {
+    yearBuilt: 2018,
+    model: "91031-XLT",
+    focalLength: 2032 
+  },
+  crew: {
+    captain: { 
+      name: 'Sandra', 
+      degree: 'Computer Engineering', 
+      encourageTeam() { console.log('We got this!') },
+     'favorite foods': ['cookies', 'cakes', 'candy', 'spinach'] }
+  },
+  engine: {
+    model: "Nimbus2000"
+  },
+  nanoelectronics: {
+    computer: {
+      terabytes: 100,
+      monitors: "HD"
+    },
+    'back-up': {
+      battery: "Lithium",
+      terabytes: 50
+    }
+  }
+}; 
+
+let capFave = spaceship.crew.captain['favorite foods'][0];  //Nested access of properties
+
+let firstPassenger = spaceship.passengers[0];
+
+
+
+let spaceships = {
+  'Fuel Type' : 'Turbo Fuel',
+  homePlanet : 'Earth'
+};
+
+// Write your code below
+
+let greenEnergy = obj => {
+  obj['Fuel Type'] = 'avocado oil';
+}
+
+let remotelyDisable = obj => {
+  obj.disabled = true;             //Reassigns the value to true
+}
+
+greenEnergy(spaceship);
+
+remotelyDisable(spaceship);
+
+console.log(spaceship)
+
+
+//Looping through objects
+
+// for...in
+for (let crewMember in spaceship.crew) {
+  console.log(`${crewMember}: ${spaceship.crew[crewMember].name}`);
+}
+
+
+let spaceshipa = {
+  crew: {
+  captain: { 
+      name: 'Lily', 
+      degree: 'Computer Engineering', 
+      cheerTeam() { console.log('You got this!') } 
+      },
+  'chief officer': { 
+      name: 'Dan', 
+      degree: 'Aerospace Engineering', 
+      agree() { console.log('I agree, captain!') } 
+      },
+  medic: { 
+      name: 'Clementine', 
+      degree: 'Physics', 
+      announce() { console.log(`Jets on!`) } },
+  translator: {
+      name: 'Shauna', 
+      degree: 'Conservation Science', 
+      powerFuel() { console.log('The tank is full!') } 
+      }
+  }
+}; 
+
+// Write your code below
+
+for (let crewMember in spaceship.crew) {
+console.log(`${crewMember}: ${spaceship.crew[crewMember].name}`)
+};
+
+for (let crewMember in spaceship.crew) {
+console.log(`${spaceship.crew[crewMember].name}: ${spaceship.crew[crewMember].degree}`)
+};
