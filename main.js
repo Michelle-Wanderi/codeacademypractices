0dc0ee6daf59307405a86800d7603f50ec8acbef
Michelle-Wanderi <michellewangari12@gmail.com>
2022-11-16 00:15:11 +0300

    Getters

--- main.js
+++ main.js
@@ -370,3 +370,18 @@ const robot = {
   }
 };
 robot.recharge ();
+
+//Getters
+const robots = {
+  _model: '1E78V2',
+  _energyLevel: 100,
+  get energyLevel(){
+    if(typeof this._energyLevel === 'number') {
+      return 'My current energy level is ' + this._energyLevel
+    } else {
+      return "System malfunction: cannot retrieve energy level"
+    }
+  }
+};
+
+console.log(robot.energyLevel);
