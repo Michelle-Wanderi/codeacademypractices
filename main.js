7ebddb1b867734e972e95825ed80c0eabc57e2a4
Michelle-Wanderi <michellewangari12@gmail.com>
2022-11-15 21:53:20 +0300

    more exercises

--- main.js
+++ main.js
@@ -348,3 +348,25 @@ console.log(`${crewMember}: ${spaceship.crew[crewMember].name}`)
 for (let crewMember in spaceship.crew) {
 console.log(`${spaceship.crew[crewMember].name}: ${spaceship.crew[crewMember].degree}`)
 };
+
+
+//Using this method
+const robot = {
+  model : '1E78V2',
+  energyLevel : 100,
+  provideInfo() {
+    return `I am ${this.model} and my current energy level is ${this.energyLevel}`
+  }
+ 
+};
+ console.log(robot.provideInfo());
+
+
+ const robott = {
+  _energyLevel: 'high',
+  recharge(){
+    this._energyLevel += 30;
+    console.log(`Recharged! Energy is currently at ${this._energyLevel}%.`)
+  }
+};
+robot.recharge ();
